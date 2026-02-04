bf624d6218e8a5a68f97fda8a80d4c2dd79d1dee
diego <diegoperezceada@gmail.com>
2026-02-04 22:11:07 +0100

    add home component

---
+++ src/app/features/movies/pages/home/home.component.ts
@@ -0,0 +1,34 @@
+import { Component, inject, signal, effect } from '@angular/core';
+import { MovieService } from '../../../../core/services/movie.services';
+import { RouterLink } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RouterLink, TranslateModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent {
+  private movieService = inject(MovieService);
+
+  movies = signal<any[]>([]);
+
+  constructor() {
+    effect(() => {
+      const currentLang = this.movieService.language();
+
+      this.loadMovies();
+    });
+  }
+
+  private loadMovies(): void {
+    this.movieService.getPopularMovies().subscribe({
+      next: (data) => {
+        this.movies.set(data);
+      },
+      error: (err) => console.error('Error cargando películas:', err),
+    });
+  }
+}
